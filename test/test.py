e3d0aef15199f97b1fe76fd3bda371aa9108395e
zhangyunfei <zhangyunfei@teach.works>
2015-04-12 15:48:40 +0800

    test

---
+++ test/test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+a='a'
+print a
+b='afafaf'
+c="""
+   afaf 
+   afaf
+   fafaf
+   """
+print c
+class A():
+    pass
+
+class B():
+    def __str__(a):
+       return "class b"
+
+print A()
+print B()
